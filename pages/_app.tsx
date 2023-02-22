05a3de0df93b29102762efcee6744ce72d9a474d
peter michael <enitanpeters28@gmail.com>
2023-02-22 10:42:20 +0100

    fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '@/styles/globals.css'
 import '@/styles/Home.css'
 import '@/styles/Contact.css'
-import '@/styles/Header.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout';
 import { wrapper } from "../store/store";
